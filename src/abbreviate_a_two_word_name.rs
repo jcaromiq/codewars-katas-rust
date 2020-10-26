d096cc1658095ad419840b60c33119d7bf1a15ab
Joaquin Caro <jcaromiq@gmail.com>
2020-10-26 23:44:47 +0100

    Abbreviate a Two Word Name kata

---
+++ src/abbreviate_a_two_word_name.rs
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/train/rust
+fn abbrev_name(name: &str) -> String {
+    // code away
+}
+
+// Rust test example:
+#[test]
+fn sample_tests() {
+    assert_eq!(abbrev_name("Sam Harris"), "S.H");
+    assert_eq!(abbrev_name("Patrick Feenan"), "P.F");
+    assert_eq!(abbrev_name("Evan Cole"), "E.C");
+    assert_eq!(abbrev_name("P Favuzzi"), "P.F");
+    assert_eq!(abbrev_name("David Mendieta"), "D.M");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@ mod sums_of_parts;
 mod string_ends_with;
 mod descending_order;
 mod number_of_people_in_the_bus;
+mod abbreviate_a_two_word_name;
